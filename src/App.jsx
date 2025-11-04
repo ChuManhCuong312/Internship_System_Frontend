d21a30a79546c32bac416e72be155fe7fe10562c
lxthanh1 <xuanthanhle999@gmail.com>
2025-11-04 10:29:53 +0700

    thêm sidebar và Dashboard với CSS thuần

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-aaa
+import React from 'react';
+import Sidebar from './components/Layout/Sidebar';
+import Dashboard from './pages/Intern/Dashboard';
+
+const App = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
