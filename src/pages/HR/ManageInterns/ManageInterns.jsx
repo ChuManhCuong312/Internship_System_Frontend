68f97f0c09f8ed146d292babdac725b2d29d74ed
Nguyen Duong <dn20050719@gmail.com>
2025-11-19 18:08:43 +0700

    fix(HR): cho phép sửa hồ sơ NO_FILE, ẩn cột trạng thái ở ManageInterns

--- src/pages/HR/ManageInterns/ManageInterns.jsx
+++ src/pages/HR/ManageInterns/ManageInterns.jsx
@@ -184,6 +184,7 @@ const ManageInterns = () => {
           onView={setViewingIntern}
           showDocuments={false}
           showApproveActions={false}
+          showStatus={false}
         />
 
         {editingIntern && (
