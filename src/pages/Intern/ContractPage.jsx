05e1612a9579326623e9e4181a0a2fa278ac890b
maixuantuan2082005 <mait2082005@gmail.com>
2025-11-22 15:51:38 +0700

    Sua duong dan file ContractList

--- src/pages/Intern/ContractPage.jsx
+++ src/pages/Intern/ContractPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import InternSidebar from '../../components/Layout/InternSidebar';
 import ContractFilter from '../../components/Contracts/ContractFilter';
-import ContractList from '../../components/Contract/ContractList';
+import ContractList from '../../components/Contracts/ContractList';
 import ContractViewer from '../../components/Contract/ContractViewer';
 import ConfirmContractModal from '../../components/Contract/ConfirmContractModal';
 import '../../styles/dashBoard.css'; // Giữ lại styles chung
