3700512abd55b5c019b22ef10a3419dea6939414
Nguyen Duong <dn20050719@gmail.com>
2025-11-23 21:36:10 +0700

    refactor(css): tổ chức lại file CSS và cập nhật import tại các trang liên quan

--- src/pages/HR/ManageInterns/component/HRInternHeader.jsx
+++ src/pages/HR/ManageInterns/component/HRInternHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "../../../../styles/manageUsers.css";
+import "../../../../styles/manageInterns.css";
+import "../../../../styles/buttons.css";
 
 const HRInternHeader = ({
   title = "Quản lý hồ sơ thực tập sinh",
