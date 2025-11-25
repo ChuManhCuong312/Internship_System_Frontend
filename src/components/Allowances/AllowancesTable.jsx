9416b4a679ce7b123af7063634d0b486721b22a4
codesomethinguseful <hdang4747@gmail.com>
2025-11-25 23:33:59 +0700

    fix: stt và bỏ cột id tts

--- src/components/Allowances/AllowancesTable.jsx
+++ src/components/Allowances/AllowancesTable.jsx
@@ -15,11 +15,12 @@ const AllowancesTable = ({
         <thead>
           <tr>
             <th onClick={() => onSort("allowanceId")}>
-              ID {sortBy === "allowanceId" && (direction === "asc" ? "↑" : "↓")}
+              STT {sortBy === "allowanceId" && (direction === "asc" ? "↑" : "↓")}
             </th>
-            <th onClick={() => onSort("internId")}>
+
+           {/* <th onClick={() => onSort("internId")}>
               ID TTS {sortBy === "internId" && (direction === "asc" ? "↑" : "↓")}
-            </th>
+            </th> */}
             <th>Tên thực tập sinh</th>
             <th onClick={() => onSort("type")}>
               Loại {sortBy === "type" && (direction === "asc" ? "↑" : "↓")}
