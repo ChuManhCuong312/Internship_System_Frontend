76afcf56f7cd69903057f71abc3f79fd607e05a7
maixuantuan2082005 <mait2082005@gmail.com>
2025-11-25 10:35:10 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,6 @@ import AllowanceRequest from "./pages/Intern/AllowanceRequest";
 import SupportRequest from "./pages/Intern/SupportRequest";
 import Calendar from "./pages/Intern/Calendar";
 import ContractPage from "./pages/Intern/ContractPage";
-import Notifications from "./pages/Intern/Notifications";
-import Allowances from "./components/Allowances/Allowances";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { HrProvider } from "./context/HrContext.jsx";
@@ -193,14 +191,6 @@ function App() {
               element={
                 <PrivateRoute allowedRoles={["INTERN"]}>
                   <ContractPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/intern/notifications"
-              element={
-                <PrivateRoute allowedRoles={["INTERN"]}>
-                  <Notifications />
                 </PrivateRoute>
               }
             />
